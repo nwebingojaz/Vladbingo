8a1bb7c740a915181ba7634be35a7031cc46c0ea
40foodorder <fooddelivery227@gmail.com>
2026-03-22 00:03:57 +0300

    Update build script for stable migrations

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
+
+# Install dependencies
 pip install -r backend/requirements.txt
-python backend/manage.py migrate
+
+# Create static directory
+mkdir -p backend/staticfiles
+
+# Collect static files
+python backend/manage.py collectstatic --no-input
+
+# Run migrations (This will now find the files we just pushed)
+python backend/manage.py migrate --no-input
