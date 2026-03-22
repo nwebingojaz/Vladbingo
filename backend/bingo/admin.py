da3aa263741cc11d18eef8f28406dfb930c75ece
40foodorder <fooddelivery227@gmail.com>
2026-03-22 13:21:06 +0300

    Admin: Registered all bingo models for the dashboard

---
+++ backend/bingo/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, PermanentCard, GameRound, Transaction
+
+# Show the custom User fields (balance and selected card)
+class CustomUserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Bingo Info', {'fields': ('operational_credit', 'selected_card', 'is_agent')}),
+    )
+    list_display = ('username', 'operational_credit', 'selected_card', 'is_staff')
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(PermanentCard)
+admin.site.register(GameRound)
+admin.site.register(Transaction)
